5df9aad4060760e6049d481081cc624893cf43cc
primer commit <lupita199>
2021-01-18 21:52:09 -0600

    preparar para servidor

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port=process.env.port || 3000;
-app.use('/public', express.static(path.join(__dirname, '/public')));
+const port = process.env.port || 3000;
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.listen(port, () => {
-    console.log("Servidor en puerto 3000");
+    console.log("Servidor en puerto 3000" + port);
 });
